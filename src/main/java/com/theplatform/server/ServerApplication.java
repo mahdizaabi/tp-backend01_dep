52e8b6b2461fe9c6ee11bf9ab61fd55f7a2fd98d
mahdizaabi <mahdi.zaabi@gmail.com>
2021-09-20 16:42:43 +0200

    fix cors

--- src/main/java/com/theplatform/server/ServerApplication.java
+++ src/main/java/com/theplatform/server/ServerApplication.java
@@ -2,9 +2,11 @@ package com.theplatform.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ServerApplication {
 
 	public static void main(String[] args) {
